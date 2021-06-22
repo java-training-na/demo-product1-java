d1a4cb08f1bbe635dddd9881cfcf21be8d68ad65
Nishant <nishant4755@gmail.com>
2021-06-22 21:25:08 +0530

    mavens demo

---
+++ src/main/java/com/nishant/product1/Application.java
@@ -0,0 +1,12 @@
+package com.nishant.product1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class Application {
+	static final Logger logger = LoggerFactory.getLogger(Application.class);
+	public static void main(String[] args) {
+		logger.debug("This is a debug message");
+		logger.info("This is a info message");
+		logger.warn("This is a warning message");
+		logger.error("This is an error message");
+	}
+}
