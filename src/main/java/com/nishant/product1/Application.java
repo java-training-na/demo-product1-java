ce915a7f2908b2879d8c1a8d5e438245ad654cb8
Nishant <nishant4755@gmail.com>
2021-06-22 22:46:25 +0530

    Changes

--- src/main/java/com/nishant/product1/Application.java
+++ src/main/java/com/nishant/product1/Application.java
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory;
 public class Application {
 	static final Logger logger = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
+		logger
 		logger.debug("This is a debug message");
 		logger.info("This is a info message");
 		logger.warn("This is a warning message");
